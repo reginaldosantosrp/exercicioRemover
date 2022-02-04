71ee812ed430bdb3bb180e2e369d55880879baad
Reginaldo Santos <reginaldo.santosrp@icloud.com>
2022-02-04 05:47:35 -0300

    remover 2 séries da nossa variável séries no arquivo app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let filmes = require('./series')
+
+
+//para adicionar novos filmes executei o comando da linha abaixo.
+
+filmes.pop('starWars A Ascenção Skywalker')
+filmes.pop('Os Ultimos Jedi')
+
+console.log(filmes)
---
+++ series.js
@@ -0,0 +1,32 @@
+let filmes = [
+
+    'starWars A Ameaça Fantasma',
+    
+    'starWars Ataque dos Clones',
+    
+    'starWars A Vingaça do Sith',
+    
+    'starWars Han Solo',
+    
+    'StarWars Rogue One',
+    
+    'StarWars Rogue One',
+    
+    'StarWars Uma Nova Esperança',
+    
+    'StarWars O Imperio Contra Ataca',
+    
+    'StarWars O Retorno de Jedi',
+    
+    'starWars O Despertar da Força',
+    
+    'starWars Os Ultimos Jedi',
+    
+    'starWars A Ascenção Skywalker',
+    
+    ];
+    
+    module.exports = filmes
+    
+    
+    
